37c7719d11e7957681977581c337eabd4ed31d50
Vladimir <v.khliupnev@haulmont.com>
2023-11-30 17:51:09 +0400

    Обновил код

--- index.ts
+++ index.ts
@@ -1,10 +1,19 @@
+export type Movie = {
+  backdrop: {
+    previewUrl: string;
+  };
+  name: string;
+};
+
 const showMoreButton = <HTMLElement>document.querySelector(".show-more");
 const preloader = <HTMLElement>document.querySelector(".spinner");
 const phone = <HTMLElement>document.getElementById("phone");
 const nameEl = <HTMLElement>document.getElementById("name");
-const callbackForm = <HTMLFormElement>document.getElementById("callback");
 const successForm = <HTMLElement>document.querySelector(".form__success");
 const closeSuccessFormButton = <HTMLElement>document.querySelector(".form__success-close");
+
+const callbackForm = <HTMLFormElement>document.getElementById("callback");
+
 const API_KEY: string = "P3RTS9G-2YH4XAV-QWKVWAZ-E9XFFDQ";
 const API_MOVIES_URL: string = "https://api.kinopoisk.dev/v1.4/movie?rating.imdb=8-10";
 
@@ -24,45 +33,62 @@ async function getMovies(url: string) {
   showMovies(respData);
 }
 
+async function loadMovie() {
+  preloader.classList.add("spinner_show");
+  await getMovies(API_MOVIES_URL);
+  preloader.classList.remove("spinner_show");
+  this.remove();
+}
+
 function showMovies(data: { docs: [] }) {
   const moviesEl = document.querySelector(".movies");
 
-  if (data.docs) {
-    data.docs.forEach((movie: { backdrop: { previewUrl: string }; name: string }) => {
-      const movieEl = document.createElement("li");
-      movieEl.classList.add("movie");
-      movieEl.innerHTML = `
-                <picture class="movie__cover">
-                    <img class="movie__image" src="${movie.backdrop.previewUrl}" alt="${movie.name}" />
-                </picture>
-                <p class="movie__title">${movie.name}</p>
-            `;
-      moviesEl.appendChild(movieEl);
-    });
-  }
+  data.docs?.forEach((movie: Movie) => {
+    const li = document.createElement("li");
+    const picture = document.createElement("picture");
+    const img = document.createElement("img");
+    const p = document.createElement("p");
+
+    li.appendChild(picture);
+    picture.appendChild(img);
+    li.appendChild(p);
+
+    li.classList.add("movie");
+    picture.classList.add("movie__cover");
+    img.classList.add("movie__image");
+    p.classList.add("movie__title");
+
+    img.src = movie.backdrop.previewUrl;
+    img.alt = movie.name;
+
+    p.append(movie.name);
+
+    moviesEl.appendChild(li);
+    picture.appendChild(img);
+  });
+}
+
+function removeError(input: Element) {
+  const parent = input.parentElement;
+
+  if (!parent.classList.contains("error")) return;
+
+  parent.querySelector(".form__error").remove();
+  parent.classList.remove("error");
+}
+
+function createError(input: Element, text: string) {
+  const parent = input.parentElement;
+  const errorLabel = document.createElement("label");
+
+  errorLabel.classList.add("form__error");
+  errorLabel.textContent = text;
+  parent.classList.add("error");
+
+  parent.append(errorLabel);
 }
 
 function validation(form: Element) {
-  function removeError(input: Element) {
-    const parent = input.parentElement;
-
-    if (parent.classList.contains("error")) {
-      parent.querySelector(".form__error").remove();
-      parent.classList.remove("error");
-    }
-  }
-
-  function createError(input: Element, text: string) {
-    const parent = input.parentElement;
-    const errorLabel = document.createElement("label");
-
-    errorLabel.classList.add("form__error");
-    errorLabel.textContent = text;
-    parent.classList.add("error");
-
-    parent.append(errorLabel);
-  }
-
   let result = true;
 
   const allInputs = form.querySelectorAll("input");
@@ -88,15 +114,7 @@ function validation(form: Element) {
 }
 
 function prefixNumber(str: string) {
-  if (str === "7") {
-    return "7 (";
-  }
-  if (str === "8") {
-    return "8 (";
-  }
-  if (str === "9") {
-    return "7 (9";
-  }
+  if (str === "9") return "7 (9";
   return "7 (";
 }
 
@@ -104,13 +122,7 @@ phone.addEventListener("input", (e) => {
   const value = (phone as HTMLInputElement).value.replace(/\D+/g, "");
   minNumberLength = 11;
 
-  let result;
-
-  if ((phone as HTMLInputElement).value.includes("+8") || (phone as HTMLInputElement).value[0] === "8") {
-    result = "";
-  } else {
-    result = "+";
-  }
+  let result = "+";
 
   for (let i = 0; i < value.length && i < minNumberLength; i++) {
     switch (i) {
@@ -143,12 +155,7 @@ nameEl.addEventListener("keydown", function (e) {
   if (e.key.match(/[0-9]/)) return e.preventDefault();
 });
 
-showMoreButton.addEventListener("click", async function () {
-  preloader.classList.add("spinner_show");
-  await getMovies(API_MOVIES_URL);
-  preloader.classList.remove("spinner_show");
-  this.remove();
-});
+showMoreButton.addEventListener("click", loadMovie);
 
 closeSuccessFormButton.addEventListener("click", function () {
   successForm.classList.remove("form__success_show");
@@ -156,9 +163,7 @@ closeSuccessFormButton.addEventListener("click", function () {
 
 callbackForm.addEventListener("submit", function (event) {
   event.preventDefault();
-
-  if (validation(this) === true) {
-    callbackForm.reset();
-    successForm.classList.add("form__success_show");
-  }
+  if (!validation(this)) return;
+  callbackForm.reset();
+  successForm.classList.add("form__success_show");
 });
--- js/index.js
+++ js/index.js
@@ -11,9 +11,9 @@ const showMoreButton = document.querySelector(".show-more");
 const preloader = document.querySelector(".spinner");
 const phone = document.getElementById("phone");
 const nameEl = document.getElementById("name");
-const callbackForm = document.getElementById("callback");
 const successForm = document.querySelector(".form__success");
 const closeSuccessFormButton = document.querySelector(".form__success-close");
+const callbackForm = document.getElementById("callback");
 const API_KEY = "P3RTS9G-2YH4XAV-QWKVWAZ-E9XFFDQ";
 const API_MOVIES_URL = "https://api.kinopoisk.dev/v1.4/movie?rating.imdb=8-10";
 let minNumberLength = 18;
@@ -30,38 +30,52 @@ function getMovies(url) {
         showMovies(respData);
     });
 }
+function loadMovie() {
+    return __awaiter(this, void 0, void 0, function* () {
+        preloader.classList.add("spinner_show");
+        yield getMovies(API_MOVIES_URL);
+        preloader.classList.remove("spinner_show");
+        this.remove();
+    });
+}
 function showMovies(data) {
+    var _a;
     const moviesEl = document.querySelector(".movies");
-    if (data.docs) {
-        data.docs.forEach((movie) => {
-            const movieEl = document.createElement("li");
-            movieEl.classList.add("movie");
-            movieEl.innerHTML = `
-                <picture class="movie__cover">
-                    <img class="movie__image" src="${movie.backdrop.previewUrl}" alt="${movie.name}" />
-                </picture>
-                <p class="movie__title">${movie.name}</p>
-            `;
-            moviesEl.appendChild(movieEl);
-        });
-    }
+    (_a = data.docs) === null || _a === void 0 ? void 0 : _a.forEach((movie) => {
+        const li = document.createElement("li");
+        const picture = document.createElement("picture");
+        const img = document.createElement("img");
+        const p = document.createElement("p");
+        li.appendChild(picture);
+        picture.appendChild(img);
+        li.appendChild(p);
+        li.classList.add("movie");
+        picture.classList.add("movie__cover");
+        img.classList.add("movie__image");
+        p.classList.add("movie__title");
+        img.src = movie.backdrop.previewUrl;
+        img.alt = movie.name;
+        p.append(movie.name);
+        moviesEl.appendChild(li);
+        picture.appendChild(img);
+    });
+}
+function removeError(input) {
+    const parent = input.parentElement;
+    if (!parent.classList.contains("error"))
+        return;
+    parent.querySelector(".form__error").remove();
+    parent.classList.remove("error");
+}
+function createError(input, text) {
+    const parent = input.parentElement;
+    const errorLabel = document.createElement("label");
+    errorLabel.classList.add("form__error");
+    errorLabel.textContent = text;
+    parent.classList.add("error");
+    parent.append(errorLabel);
 }
 function validation(form) {
-    function removeError(input) {
-        const parent = input.parentElement;
-        if (parent.classList.contains("error")) {
-            parent.querySelector(".form__error").remove();
-            parent.classList.remove("error");
-        }
-    }
-    function createError(input, text) {
-        const parent = input.parentElement;
-        const errorLabel = document.createElement("label");
-        errorLabel.classList.add("form__error");
-        errorLabel.textContent = text;
-        parent.classList.add("error");
-        parent.append(errorLabel);
-    }
     let result = true;
     const allInputs = form.querySelectorAll("input");
     for (const input of allInputs) {
@@ -82,27 +96,14 @@ function validation(form) {
     return result;
 }
 function prefixNumber(str) {
-    if (str === "7") {
-        return "7 (";
-    }
-    if (str === "8") {
-        return "8 (";
-    }
-    if (str === "9") {
+    if (str === "9")
         return "7 (9";
-    }
     return "7 (";
 }
 phone.addEventListener("input", (e) => {
     const value = phone.value.replace(/\D+/g, "");
     minNumberLength = 11;
-    let result;
-    if (phone.value.includes("+8") || phone.value[0] === "8") {
-        result = "";
-    }
-    else {
-        result = "+";
-    }
+    let result = "+";
     for (let i = 0; i < value.length && i < minNumberLength; i++) {
         switch (i) {
             case 0:
@@ -131,21 +132,15 @@ nameEl.addEventListener("keydown", function (e) {
     if (e.key.match(/[0-9]/))
         return e.preventDefault();
 });
-showMoreButton.addEventListener("click", function () {
-    return __awaiter(this, void 0, void 0, function* () {
-        preloader.classList.add("spinner_show");
-        yield getMovies(API_MOVIES_URL);
-        preloader.classList.remove("spinner_show");
-        this.remove();
-    });
-});
+showMoreButton.addEventListener("click", loadMovie);
 closeSuccessFormButton.addEventListener("click", function () {
     successForm.classList.remove("form__success_show");
 });
 callbackForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    if (validation(this) === true) {
-        callbackForm.reset();
-        successForm.classList.add("form__success_show");
-    }
+    if (!validation(this))
+        return;
+    callbackForm.reset();
+    successForm.classList.add("form__success_show");
 });
+export {};
